212f0df2af91fbd57ded1d60438547b4e36e5e62
Bozatzis Miltiadis <mpozan20@gmail.com>
2022-08-24 11:51:15 +0200

    add function that calculate the states in a quantum harmonic oscillator

--- potentials_1D/shooting.py
+++ potentials_1D/shooting.py
@@ -47,42 +47,56 @@ def findEnergyEigenValues(func, psi0, x, V, E_arr):
         eigenEnergies.append(newton(shootOneE, E_arr[z], args = (func, psi0, x, V)))
     return np.asarray(eigenEnergies)
 
+
 def shootInfinitePotentialWell(psi0, dx, a, V0, E_arr):
-    x = np.arange(0.0, a + dx, dx)
-    V = []
-    for _ in x:
-        V.append(V0)
-    eigE = findEnergyEigenValues(TimeIndependentSE, psi0, x, V, E_arr)
+    x_ipw = np.arange(0.0, a + dx, dx)
+    V_ipw = []
+    for _ in x_ipw:
+        V_ipw.append(V0)
+    eigE = findEnergyEigenValues(TimeIndependentSE, psi0, x_ipw, V_ipw, E_arr)
     ipw_out = []
     for E in eigE:
-        out = rk4(TimeIndependentSE, psi0, x, V, E)
+        out = rk4(TimeIndependentSE, psi0, x_ipw, V_ipw, E)
         ipw_out.append(normalize(out [: , 0]))
     out_arr = np.asarray(ipw_out)
-    return x, out_arr
+    return x_ipw, out_arr
 
 def shootFinitePotentialWell(psi0, dx, x_range, a, V0, E_arr):
-    x_arr_pw = np.arange(-x_range, x_range + dx, dx)
+    x_pw = np.arange(-x_range, x_range + dx, dx)
     V_pw = []
-    for x in x_arr_pw:
+    for x in x_pw:
         if x < -a/2.0 or x > a/2.0:
             V_pw.append(0) 
         else:
             V_pw.append(V0)
-    eigE = findEnergyEigenValues(TimeIndependentSE, psi0, x_arr_pw, V_pw, E_arr)
+    eigE = findEnergyEigenValues(TimeIndependentSE, psi0, x_pw, V_pw, E_arr)
     pw_out = []
-    for EE in eigE:
-        out = rk4(TimeIndependentSE, psi0, x_arr_pw, V_pw, EE)
+    for E in eigE:
+        out = rk4(TimeIndependentSE, psi0, x_pw, V_pw, E)
         pw_out.append(normalize(out[: , 0]))
     out_arr = np.asarray(pw_out)
-    return x_arr_pw, out_arr
+    return x_pw, out_arr
+
+def shootQuantumHarmonicOscillator(psi0, dx, x_range, V_offset, E_arr):
+    x_qho = np.arange(-x_range, x_range + dx, dx)
+    V_qho = []
+    for x in x_qho:
+        V_qho.append(V_offset + x**2)
+    eigE = findEnergyEigenValues(TimeIndependentSE, psi0, x_qho, V_qho, E_arr)
+    pw_out = []
+    for E in eigE:
+        out = rk4(TimeIndependentSE, psi0, x_qho, V_qho, E)
+        pw_out.append(normalize(out[: , 0]))
+    out_arr = np.asarray(pw_out)
+    return x_qho, out_arr
 
 def main():
     psi_0 = 0.0
     phi_0 = 1.0
     psi0 = np.array([psi_0, phi_0])
     dx = 0.001
-    E = np.arange(-25, 0, 1.0)
-    x, states = shootFinitePotentialWell(psi0, dx, 4, 1, -25, E)
+    E = np.arange(-5, 0, 1.0)
+    x, states = shootQuantumHarmonicOscillator(psi0, dx, 5, -5, E)
     for state in states:
         plt.plot(x, state)
     plt.show()
